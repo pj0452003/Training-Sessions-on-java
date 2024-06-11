4e29231fa775ec8fc023068f79939be20216a1b0
Pritam Jirage <163889796+pj0452003@users.noreply.github.com>
2024-06-11 15:05:28 +0530

    Add files via upload

---
+++ FunctionDemo.java
@@ -0,0 +1,15 @@
+package sam.com;
+
+public class FunctionDemo {
+	public static void add() {
+		int a=22;
+		int b=45;
+		int total = a+b;
+		System.out.println("addition = " +total);
+	}
+	public static void sub(int a, int b) {
+		int total = a-b;
+		System.out.println("Substraction is = "+total);
+		
+	}
+}
---
+++ TestFunction.java
@@ -0,0 +1,13 @@
+package sam.com;
+
+public class TestFunction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hello");
+		//FunctionDemo fd=new FunctionDemo(); //Object creation
+		FunctionDemo.add();
+		FunctionDemo.sub(100, 78);
+	}
+
+}
